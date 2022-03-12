53576c56703193801510436c06418c7072d9fea0
hemil patel <gentindian@hotmail.com>
2022-03-12 11:02:23 -0800

    Initial Commit

---
+++ dummy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dummy
+//
+//  Created by Hemil Patel on 3/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dummy/dummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummyApp.swift
+//  dummy
+//
+//  Created by Hemil Patel on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct dummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
